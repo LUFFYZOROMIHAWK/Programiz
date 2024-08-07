16df4678f662917c5553fb2d84cc7a450baa0895
Anirudh T. <leetiger753@gmail.com>
2024-08-07 19:09:41 +0530

    Add 3.c.

---
+++ C Programming For Beginners/3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+
+    double number = 12.45;
+    float number1 = 10.9f;
+    double number2 = 5.5e6;
+    printf("%.2lf\n", number);
+    printf("\n%f", number1);
+    printf("\n%lf", number2);
+
+    return 0;
+}
