45c786a84666d41d09cd9fe6bbe20e2561315e32
Anirudh T. <leetiger753@gmail.com>
2024-10-21 21:48:16 +0530

    Add 16.c.

---
+++ C Programming For Beginners/16.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int num=25;
+    int cube=27;
+    int a=5;
+    int b=2;
+    double result=pow(a,b);
+    printf("Square root is:%lf\n",sqrt(num));
+    printf("Cube root is:%lf\n",cbrt(cube));
+    printf("Power is: %lf",result);
+    return 0;
+}
