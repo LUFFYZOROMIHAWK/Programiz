abfcdeffa11b83b0565b1248fd7dfc49183e20b0
Anirudh T. <leetiger753@gmail.com>
2024-10-23 06:17:16 +0530

    Add 18.c.

---
+++ C Programming For Beginners/18.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int age[5];
+    scanf("%d",&age[0]);
+    scanf("%d",&age[1]);
+    scanf("%d",&age[2]);
+    scanf("%d",&age[3]);
+    scanf("%d",&age[4]);
+    printf("Age 1:",age[0]);
+    printf("Age 2:",age[1]);
+    printf("Age 3:",age[2]);
+    printf("Age 4:",age[3]);
+    printf("Age 5:",age[4]);
+    return 0;
+}
