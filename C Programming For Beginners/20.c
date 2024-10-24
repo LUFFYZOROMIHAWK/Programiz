01e558979c672d829a91c26f567665246e5e7dfa
Anirudh T. <leetiger753@gmail.com>
2024-10-24 15:24:07 +0530

    Add 20.c.

---
+++ C Programming For Beginners/20.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char str[20];
+    printf("Enter your name in the format (FN).(LN):");
+    scanf("%s",&str);
+    printf("Your name is %s",str);
+    return 0;
+}
